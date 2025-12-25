68d1c05eb44021de3ad5a193423488f301dcfce8
knamle <khanh.le@epfl.ch>
2025-12-25 21:54:33 +0100

    wrote init function and part of emulatecycle. Chose to have function pointer array instead of many switch cases for opcode handling

--- chip8.cpp
+++ chip8.cpp
@@ -1,10 +1,47 @@
 #include "chip8.hpp"
 
-void chip8::initialize() {};
+void chip8::initialize() {
+    pc     = 0x200;  // Program counter starts at 0x200
+    opcode = 0;      // Reset current opcode	
+    I      = 0;      // Reset index register
+    sp     = 0;      // Reset stack pointer
+    
+    // TODO: Clear display	
+    
+    // Clear stack
+    // Clear registers V0-VF
+    for (int i = 0; i < 16; ++i) {
+        stack[i] = 0;
+        V[i] = 0;
+    }
+
+    // Clear memory
+    for (int i = 0; i < 4096; ++i) memory[i] = 0; 
+    
+    // Load fontset
+    for(int i = 0; i < 80; ++i) memory[i] = chip8_fontset[i];		
+    
+    // Reset timers
+    delay_timer = 0;
+    sound_timer = 0;
+
+    // init function pointers
+    using Fn = void(*)();
+    Fn table[16]; // 0xxx - Fxxx
+}
+
+void chip8::emulateCycle() {
+    // Fetch Opcode
+    opcode = memory[pc] << 8 | memory[pc + 1]; // where does pc get updated?
+
+    // Decode Opcode
+    
+    // Execute Opcode
+
+    // Update timers
+}
 
 void chip8::loadGame(std::string s) {};
-
-void chip8::emulateCycle() {};
 
 void chip8::setKeys() {};
 
